b2f9bb7cafefa7795404e9298be0fc6cae313c26
ppabis <piotr@pabis.eu>
2023-07-10 22:11:59 +0200

    Trigger pipeline function

--- branches.py
+++ branches.py
@@ -1,4 +1,7 @@
 import requests, os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
+
 PROJECT_ID = 47513328 # Bring your own project ID
 API_URL = "https://gitlab.com/api/v4/" # For self-hosted, change this
 GITLAB_TOKEN = os.environ["GITLAB_TOKEN"] # Will fail if not set
@@ -21,3 +24,15 @@ def get_pipelines():
     url = f"{API_URL}/projects/{PROJECT_ID}/pipelines?per_page=50"
     headers = {"PRIVATE-TOKEN": GITLAB_TOKEN}
     return requests.get(url, headers=headers).json()
+
+def trigger_pipeline(branch, build_type):
+    url = f"{API_URL}/projects/{PROJECT_ID}/trigger/pipeline"
+    headers = {"PRIVATE-TOKEN": GITLAB_TOKEN}
+    body = {
+        "ref": branch,
+        "variables": [
+            { "key": 'BUILD_TYPE', "value": build_type }
+        ]
+    }
+    response = requests.post(url, headers=headers, json=body)
+    return (response.status_code, response.json())
